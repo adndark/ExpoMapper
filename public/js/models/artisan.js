92206283c40a0ebd9878e20650f2a3f1409e2518
adndark <adndark@gmail.com>
2019-07-08 22:03:38 -0700

    Move artisan model to its own file and restructure files

---
+++ public/js/models/artisan.js
@@ -0,0 +1,23 @@
+// Defiine artisan model
+const ArtisanModel = Backbone.Model.extend({
+    // Url to fetch data from
+    urlRoot: "/api/artisans",
+});
+
+// Define artisan collection which retrives data for
+// a given user id
+const ArtisanCollection = Backbone.Collection.extend({
+
+    // Override initialize
+    initialize: function(models, options) {
+        this.userId = options.userId;
+    },
+
+    // Define model
+    model: ArtisanModel,
+
+    // Override url function
+    url: function() {
+        return "/api/artisansByUserId/" + this.userId;
+    }
+});
